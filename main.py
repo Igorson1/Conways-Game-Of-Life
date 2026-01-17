e2d8d98a2dcfeb89a4dd7dec3dacaaf4730fc959
dejm <dejm@pc-590.home>
2026-01-18 00:44:04 +0100

    dodanie logiki gry

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import pygame
 import copy
 
+def liczba_zywych_sasiadow(siatka, x, y, rozmiar):
+    """
+    funkcja sprawdza 8 sąsiednich komórek dla komórki (x; y) - zwraca liczbę żywych
+    funkcja nie sprawdza pól poza granicą siatki
+    """
+    suma = 0
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if i == 0 and j == 0:
+                continue # bez uwzględniania aktualnie sprawdzanej komórki
+            
+            nx, ny = x + i, y + j
+            
+            # warunek żeby nie wyjść poza listę
+            if 0 <= nx < rozmiar and 0 <= ny < rozmiar:
+                suma += siatka[nx][ny]
+    return suma
+
 def main():
 
     pygame.init()
@@ -17,8 +35,11 @@ def main():
     # 0 0 0
 
     siatka = [[0 for x in range(rozmiar_planszy)] for y in range(rozmiar_planszy)]
+    
+    #przykladowy przypadek:
     siatka[27][27] = 1
-
+    siatka[26][27] = 1
+    siatka[25][27] = 1
 
     screen = pygame.display.set_mode((wielkosc_komorki*rozmiar_planszy,wielkosc_komorki*rozmiar_planszy))
     clock = pygame.time.Clock()
@@ -29,6 +50,7 @@ def main():
                 pygame.quit()
                 raise SystemExit
 
+        
         # Sekcja logiki
 
         #############################################
@@ -36,7 +58,21 @@ def main():
 
         temp_siatka = copy.deepcopy(siatka)
 
-        #tu pisz logike programu
+        # przejście przez każdą komórkę i zadecydowanie czy żyje czy jest martwa
+        for x in range(rozmiar_planszy):
+            for y in range(rozmiar_planszy):
+                sasiedzi = liczba_zywych_sasiadow(siatka, x, y, rozmiar_planszy)
+                
+                if siatka[x][y] == 1: # jesli żyje
+                    if sasiedzi < 2 or sasiedzi > 3:
+                        temp_siatka[x][y] = 0 # umiera gdy ma mniej niż 2 i więcej niż 3 sąsiadów
+                else: # jeśli jest martwa
+                    if sasiedzi == 3:
+                        temp_siatka[x][y] = 1 # rodzi się dla 3 sąsiadów
+
+        # nadpisanie starej siatki nową
+        siatka = copy.deepcopy(temp_siatka)
+
 
         #############################################
 
@@ -57,5 +93,5 @@ def main():
 
         ##############################################
 
-        clock.tick(30)         # wait until next frame (at 60 FPS
+        clock.tick(5)         # wait until next frame (at 60 FPS
 main()
